2a401ad72db9e265247d10f672e44a830b1fec36
rizpedia <achmadrizkiramadhan0101@gmail.com>
2022-08-25 12:25:40 +0700

    fix bug remove number

--- hapusNomer.php
+++ hapusNomer.php
@@ -8,20 +8,18 @@
         $lenghtArrayData = count($arraydaftar);
         if( $lenghtArrayData = 1){
             array_splice($arraydaftar, $datarespon, 1); 
-            // unset($arraydaftar[$datarespon]); 
             $array['daftar'] = $arraydaftar;
             var_dump($arraydaftar);
-            // var_dump($array);
             file_put_contents('./assets/data/nomer.json', json_encode($array));
             header('Location: index.html');
+            exit();
         }else{
             array_splice($arraydaftar, $datarespon, $datarespon); 
-            // unset($arraydaftar[$datarespon]); 
             $array['daftar'] = $arraydaftar;
             var_dump($arraydaftar);
-            // var_dump($array);
             file_put_contents('./assets/data/nomer.json', json_encode($array));
             header('Location: index.html');
+            exit();
         }
 
 }
