e457cd59c4ad46b46647e1782539a43324ee0f32
rizpedia <achmadrizkiramadhan0101@gmail.com>
2022-08-25 17:47:14 +0700

    margin file controller

---
+++ controller.php
@@ -0,0 +1,43 @@
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(isset($_POST['nomeraktif'])){
+            $datarespon = $_POST['nomeraktif'];
+            $data = file_get_contents("./assets/data/nomer.json");
+            $array = json_decode($data, true);
+            $array['aktif'] = $datarespon;
+            file_put_contents('./assets/data/nomer.json', json_encode($array));
+            header('Location: index.html');
+            exit();
+        }
+        if(isset($_POST['tambahnomer'])){
+            $tambahnomer = $_POST['tambahnomer'];
+            $data = file_get_contents("./assets/data/nomer.json");
+            $array = json_decode($data, true);
+            $array['daftar'][] = $tambahnomer;
+            file_put_contents('./assets/data/nomer.json', json_encode($array));
+            header('Location: index.html');
+            exit();
+        }
+        if(isset($_POST['hapusnomer'])){
+            $datarespon = $_POST['hapusnomer'];
+            $data = file_get_contents("./assets/data/nomer.json");
+            $array = json_decode($data, true);
+            $arraydaftar = $array['daftar'];
+            $lenghtArrayData = count($arraydaftar);
+            if( $lenghtArrayData = 1){
+                array_splice($arraydaftar, $datarespon, 1); 
+                $array['daftar'] = $arraydaftar;
+                file_put_contents('./assets/data/nomer.json', json_encode($array));
+                header('Location: index.html');
+                exit();
+            } else {
+                array_splice($arraydaftar, $datarespon, $datarespon); 
+                $array['daftar'] = $arraydaftar;
+                file_put_contents('./assets/data/nomer.json', json_encode($array));
+                header('Location: index.html');
+                exit();
+            }
+        }
+        
+}
+?>
